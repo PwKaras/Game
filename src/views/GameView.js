a8a911017b5deb707a84c2490f878f125a433a3c
Paweł Karaś <pwkaras@gmail.com>
2020-10-21 22:10:01 +0200

    gameView - buttons animation type generator -  avoiding repeated value, clean effect on useEffect,  score counter

--- src/views/GameView.js
+++ src/views/GameView.js
@@ -5,30 +5,49 @@ import Header from '../components/Header';
 const GameView = () => {
 
     const [animationType, setAnimationType] = useState("jump");
+    const [counter, setCounter] = useState(0);
 
+    // randorm changing button`s annimation type
     useEffect(
         () => {
             const id = setTimeout(
                 () => {
-                   const animationTypeArr = ["jump","rightEnter","leftEnter","square",]
-                   const randomIndex = Math.floor(Math.random()*4)
-                    setAnimationType(animationTypeArr[randomIndex]
-                    )
+                    const animationTypeArr = ["jump", "rightEnter", "leftEnter", "square"];
+                    const randomIndex = Math.floor(Math.random() * 4);
+                    let newAnimationType = animationTypeArr[randomIndex];
+
+                    // avoiding repeated random values
+                    if (animationType === newAnimationType) {
+                        animationType === "jump" ?
+                            setAnimationType("rightEnter") :
+                            setAnimationType(animationTypeArr[randomIndex - 1])
+                    } else {
+                        setAnimationType(newAnimationType)
+                    };
                 }, 3000
-            )
+            );
+            // clean up effect
+            return () => {
+                clearTimeout(id);
+            };
         }, [animationType]
-    )
-    console.log(animationType);
+    );
+
+    const scoreHandler = () => {
+        setCounter(counter + 1);
+    };
+
     return (
         <>
             <Header />
             <div className="App">
                 <header className="App-header">
                     <h2>
-                        Game will be here
+                        My score {counter}
         </h2>
                     <Button
-                    animation={`button-animation__${animationType}`}>CATCH ME</Button>
+                        onClick={scoreHandler}
+                        animation={`button-animation__${animationType}`}>CATCH ME</Button>
                 </header>
 
             </div>
